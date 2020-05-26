ac71805947ac730a0b51d694fa4d4ffbdeba7f43
Anatoliy Varankin <varankin@elewise.com>
2020-05-26 14:14:27 +0400

    feat: сверстать разметку

---
+++ script.js
@@ -0,0 +1,13 @@
+const todoList = [
+  "Сделать проектную работу",
+  "Полить цветы",
+  "Пройти туториал по Реакту",
+  "Сделать фронт для своего проекта",
+  "Погулять с собакой",
+  "Разобраться в замыканиях",
+  "Решить задачу на Codewars",
+];
+const itemTemplate = document.querySelector(".item_template").content;
+const list = document.querySelector(".list");
+const form = document.querySelector(".form");
+const formInput = document.querySelector(".form__input");
